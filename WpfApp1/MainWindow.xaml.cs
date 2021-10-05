2e7197f7f5d26fbe6ab42dceffc0c871768fb468
ivan <ivan@DESKTOP-SQRQOA1>
2021-10-05 18:07:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Level { set; get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings S = new Settings();
+            S.Owner = this;
+            //S.Show();
+            if (S.ShowDialog() ==  true) 
+            {
+                Level = S.Level; Label1.Content = "" + Level; 
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ WpfApp1/Settings.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        int level = 1;
+        public int Level { get { return level; } }
+
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (RadioButton1.IsChecked == true) { level = 1; }
+            if (RadioButton2.IsChecked == true) { level = 2; }
+            if (RadioButton3.IsChecked == true) { level = 3; }
+            this.DialogResult = true;
+        }
+    }
+}
